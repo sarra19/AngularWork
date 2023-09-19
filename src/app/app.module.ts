5c16ae5a4412da53b31988c8bd69ff9f34a995dd
sarra19 <sarra.benhamida@esprit.tn>
2023-09-20 00:18:35 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { FormsModule } from '@angular/forms';
+import { FournisseurComponent } from './fournisseur/fournisseur.component';
+import { ListFournisseurComponent } from './list-fournisseur/list-fournisseur.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    FournisseurComponent,
+    ListFournisseurComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
